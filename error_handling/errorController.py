bdeb9e8dc5457ebeb04e2deb3d2ee3ead6d5dc78
AsteriskAmpersand <a20122661@pucp.pe>
2024-08-31 10:33:38 +0200

    Fixed typo

--- error_handling/errorController.py
+++ error_handling/errorController.py
@@ -224,7 +224,7 @@ class DebugOptions():
         self.fcurve_error = addonOptions.fcurve_error
         self.action_error = addonOptions.action_error
         self.graph_error = addonOptions.graph_error
-        self.expor_hHidden = True
+        self.expor_hidden = True
 
 class DebugVerifier(ErrorHandler):
     def __init__(self,options):
